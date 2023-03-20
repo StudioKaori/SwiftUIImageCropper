25de0d3cc7c67a9f6278d5a370c555996a1a3fd3
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-20 14:45:07 +0100

    Initial Commit

---
+++ SwiftUIImageCropper/SwiftUIImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageCropperApp.swift
+//  SwiftUIImageCropper
+//
+//  Created by Kaori Persson on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
